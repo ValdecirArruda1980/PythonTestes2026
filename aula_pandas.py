a63d7a9e2ad929175ab78553a8eaf8fb396c9f70
ValdecirArruda1980 <valdecirrogerio@gmail.com>
2026-01-06 20:17:02 -0300

    Meu código Pandas enviado pelo terminal

---
+++ aula_pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Simulando dados de Engenharia de Software
+dados = {
+    'Materia': ['Calculo', 'Python', 'SQL', 'Pandas'],
+    'Nota': [8.5, 9.0, 9.5, 10.0]
+}
+
+df = pd.DataFrame(dados)
+print("Relatório de Estudos - Valdecir 2026")
+print(df)
